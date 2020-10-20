fdd5515ecce33117de102ea2c1dc5187a53931d2
Abhilasha339027 <kumaranil4971@gmail.com>
2020-10-20 21:18:21 +0530

    order

--- routes/web.js
+++ routes/web.js
@@ -15,7 +15,9 @@ function initRoutes(app){
     app.get('/cart', cartController().index)
     app.post('/update-cart', cartController().update)
 
+    //
     app.post('/orders', orderController().store)
+    app.get('/customer/orders', orderController().index)
 
 }
 
